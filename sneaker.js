694966c92b63e16d59482122a53d28638bb37fc5
Roamin999 <146077119+Roamin999@users.noreply.github.com>
2023-12-06 11:51:08 +0100

    [ADD] sneaker.js

---
+++ sneaker.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import fs from 'fs';
+const fetchData = async (page) => {
+    const url = `http://54.37.12.181:1337/api/sneakers?pagination%5Bpage%5D=${page}&pagination%5BpageSize%5D=100`;
+
+    try {
+        const response = await axios.get(url);
+        const data = response.data;
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        return null;
+    }
+};
+
+const totalPages = 500;
+const allData = [];
+
+const fetchAllData = async () => {
+    for (let page = 1; page <= totalPages; page++) {
+        const data = await fetchData(page);
+        if (data) {
+            allData.push(data);
+        }
+    }
+
+    const jsonData = JSON.stringify(allData, null, 2);
+    fs.writeFileSync('sneakerData.json', jsonData);
+    console.log('All data saved to sneakersData.json');
+};
+
+fetchAllData();
