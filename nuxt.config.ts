6eabef0bb3afec4b81b6419e90fd8ce33b3e7fb8
Roamin999 <146077119+Roamin999@users.noreply.github.com>
2023-12-08 16:36:40 +0100

    [BUILD] @nuxtjs/strapi + module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+// config de base pour strapi //
+// https://nuxt.com/docs/api/configuration/nuxt-config 
+// export default defineNuxtConfig({ 
+//   devtools: { enabled: true }
+// })
+
+// nuxt.config.js //
+export default {
+  modules: ["@nuxtjs/strapi"]
+};
