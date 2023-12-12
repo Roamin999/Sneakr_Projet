0849192f39b6e97499466417af0f851db609326e
Roamin999 <146077119+Roamin999@users.noreply.github.com>
2023-12-13 00:17:52 +0100

    [UPD] Component import to Index

--- pages/Index.vue
+++ pages/Index.vue
@@ -1,9 +1,20 @@
 <template>
-</template>
 
-<script>
-</script>
+<SneakerIndex/>
 
-<style>
-</style>
-
+  </template>   
+  
+  <script>
+  import SneakerIndex from '~/components/sneakerIndex.vue';
+  
+  </script>
+  
+  <style>
+html, body{
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: large;
+    color: #cbd5e1;
+    background-color: #111827;
+}
+  </style> 
+  
